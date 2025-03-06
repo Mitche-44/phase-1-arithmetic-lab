2515a293677c3e58cc92f55609e859c66c632da9
+mitchellengetich <+mitchellngetich24@gmail.com>
2025-03-06 17:34:56 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 =31;
+let num2 =2;
+let multiply = num1 *num2 ; multiply =62
+
+ 
+let random = Math.floor(Math.random()*100) +1;
+
+let num3=24; 
+let num4 =10; 
+let mod =num3 % num4; mod = 4 ;
+
+let set = [5,10,20,15];let max =Math.max(...set); max = 20 ;
